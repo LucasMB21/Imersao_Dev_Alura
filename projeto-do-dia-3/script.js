cd89082e3e6c2af0c47d52403c339aab5f8ce9bc
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2025-04-03 20:09:03 -0300

    update script.js

--- projeto-do-dia-3/script.js
+++ projeto-do-dia-3/script.js
@@ -1,25 +1,19 @@
-rodada = 1;
-while(rodada <= 3) {
-    console.log("Rodada:" + rodada);
+ganhou = 0; //se for 1, perdeu. se for 0, ganhou. 
 
-    escolhaJogador = prompt("Nível "+rodada+", vidro (1,2 ou 3)?");
+for(let rodada = 1; rodada <= 3; rodada++) {
+    escolhaJogador = prompt("Nível " + rodada + ": Escolha um vidro (1, 2 ou 3)?");
     pisoQuebrado = Math.floor(Math.random()*3) + 1;
 
     if(escolhaJogador == pisoQuebrado) {
         //escolheu o vidro que estava quebrado
-        alert("Vidro quebrou! Acabou o jogo pra você");
+        alert("Que pena! O vidro " + escolhaJogador + " quebrou. você perdeu!");
         rodada = 1000;
+        ganhou = 1;
+    } else {
+        alert("Bom salto! Você passou para o próximo nível.");
     }
-
-    else {
-        alert("Passou! Piso quebrado estava na ponte: " + pisoQuebrado);
-    }
-
-    rodada = rodada + 1;
 }
 
-// veio para cá quando rodada >= 4
-
-if(rodada == 4) {
-    alert("Você venceu! parabéns");
+if(ganhou == 0) {
+    alert("Parabéns! Você completou o desafio!");
 }
